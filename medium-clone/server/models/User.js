fc75701e89b868023801485ea7c819c7b09f7941
Rainier G <rainier.f.go@gmail.com>
2018-08-13 10:57:54 -0400

    Added Article & User schema and eslint to repo

---
+++ medium-clone/server/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+let UserSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    provider: String,
+    provider_id: String,
+    token: String,
+    provider_pic: String,
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  }
+);
+
+UserSchema.methods.follow = function(userId) {
+  if (this.following.indexOf(userId) === -1) {
+    this.following.push(user_id);
+  };
+  return this.save();
+};
+
+UserSchema.methods.addFollower = function(fs) {
+
+};
+
+module.exports = mongoose.model('User', UserSchema);
