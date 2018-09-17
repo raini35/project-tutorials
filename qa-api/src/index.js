c4beeb9394e08a1ffe362f1a835cb6b4c5b95964
Rainier G <rainier.f.go@gmail.com>
2018-09-17 14:15:10 -0500

    Added 5 dependencies and configured express file

---
+++ qa-api/src/index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+const questions = [];
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(morgan('combined'));
+
+app.get('/', (req,res) => {
+  const qs = questions.map((q) => ({
+    id: q.id,
+    title: q.title,
+    description: q.description,
+    answers: q.answers.length
+  }));
+
+  res.send(qs);
+});
+
+app.get('/:id', (req, res) => {
+  const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+  if (question.length > 1) return res.status(500).send();
+  if (question.length === 0) return res.status(404).send();
+  res.send(question[0]);
+});
+
+app.post('/', (req, res) => {
+  console.log("Posting /");
+  console.log("req.body: " + req.body);
+  const {title, description} = req.body;
+  console.log("title: " + title)
+  console.log("description: " + description);
+
+  const newQuestion = {
+    id: questions.length + 1,
+    title,
+    description,
+    answers: [],
+  };
+  questions.push(newQuestion);
+  res.status(200).send();
+});
+
+app.post('/answer/:id', (req, res) => {
+  console.log("Posting /answer/:id");
+  console.log("req.body: " + req.body);
+  const {answer} = req.body;
+  console.log("{answer}: " + answer);
+
+  const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+  if(question.length > 1) return res.status(500).send();
+  if(question.length === 0) return res.status(404).send();
+
+  question[0].answers.push({
+    answer,
+  });
+
+  res.status(200).send(); 
+});
+
+app.listen(8081, () => {
+  console.log('listening on port 8001');
+});
