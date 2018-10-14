221f13ae43780135dbf0fb4c2c49b74ca81b37be
Rainier G <rainier.f.go@gmail.com>
2018-10-13 21:47:52 -0500

    Finished basic layout of dashboard

--- css-grid/src/App.js
+++ css-grid/src/App.js
@@ -19,15 +19,44 @@ class App extends Component {
 
     return (
       <div class="wrapper">
-        <div className="navbar"><h1 style={{"margin-left":"20px"}}>GCal Analyzer</h1></div>
+        <div className="navbar">
+          <h1 style={{"margin-left":"20px"}}>GCal Analyzer</h1>
+        </div>
+
         <div className="calbar">
-          <div className="calchart"></div>
+          <div className="calchart">
+          </div>
           <div className="calnames">
             <h2 style={{"margin-left":"20px", "font-weight": "800"}}>My Calendars</h2>
               {listOfNames}
           </div>
         </div>
-        <div className="calanalysis"><h2 style={{"margin-left":"20px"}}>Calendar Analysis</h2></div>
+
+        <div className="calanalysis" style={{"text-align":"center"}}>
+            <div className="squares">
+              <div className="tile" style={{"background-color":"rgb(60,60,60)"}}>
+                <h3>General Info 1</h3>
+              </div>
+              <div className="tile" style={{"background-color":"rgb(60,60,60)"}}>
+                <h3>General Info 2</h3>
+              </div>
+              <div className="tile" style={{"background-color":"rgb(60,60,60)"}}>
+                <h3>General Info 3</h3>
+              </div>
+            </div>
+            <div className="rectangles">
+              <div className="tile" style={{"background-color":"rgb(60,60,60)"}}>
+                <h3>Chart 1</h3>
+              </div>
+
+              <div className="tile" style={{"background-color":"rgb(60,60,60)"}}>
+                <h3>Chart 2</h3>
+              </div>
+            </div>
+            <div style={{"background-color":"rgb(60,60,60)", "height":"500px"}}>
+              <h3>Table</h3>
+            </div>
+        </div>
       </div>
     );
   }
