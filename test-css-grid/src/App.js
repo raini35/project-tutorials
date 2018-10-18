cd43eb069122b8d0c0c8447ece95b72d96c14828
Rainier G <rainier.f.go@gmail.com>
2018-10-17 20:59:06 -0500

    Testing css grid

---
+++ test-css-grid/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Calendar from "./components/Calendar";
+
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <main>
+          <Calendar />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
